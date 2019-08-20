169cc9a5ecca5c5c500d3c9043fbcf8473ae3d9f
Bora Sumer <solasguitarra@hotmail.com>
2019-08-20 19:08:00 -0400

    build added start script for server to packaga.json

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import BookContextProvider from './contexts/BookContext';
 
 //ApolloClient Setup
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://intense-scrubland-62590.herokuapp.com/graphql",
 });
 
 function App() {
