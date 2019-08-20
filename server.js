0bacd670853e5dee434c5838198c5793a2759a58
Bora Sumer <solasguitarra@hotmail.com>
2019-08-20 18:30:52 -0400

    build added start script for server to packaga.json

--- server.js
+++ server.js
@@ -28,16 +28,13 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }));
 
-//Serve Static Assests if in production
-
-if (process.env.NODE_ENV === production) {
-  //Set a static folder
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
-
 
 app.listen(process.env.PORT || 4000, () => {
   console.log("Listening for requests on port 4000");
